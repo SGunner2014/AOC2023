8256e848e9c9321238488e9483f23b3b99f3aa86
Sam Gunner <me@sgunner.me>
2023-12-04 23:23:57 +0000

    Part 2 day 2

---
+++ day2/part2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+
+#define INPUT_FILE "input.txt"
+
+#define MIN_RED 12
+#define MIN_GREEN 13
+#define MIN_BLUE 14
+
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+
+int read_game(char *line, ssize_t read)
+{
+    int gameId = 0;
+    int i;
+    int ballColour, currentCount;
+
+    // Read the game ID
+    for (i = 5; i < read; i++) {
+        if (line[i] >= '0' && line[i] <= '9') {
+            gameId *= 10;
+            gameId += (line[i] - '0');
+        } else {
+            break;
+        }
+    }
+
+    currentCount = 0;
+    ++i;
+
+    int maxRed, maxGreen, maxBlue;
+    maxRed = maxGreen = maxBlue = 0;
+
+    for (; i < read; i++) {
+        // Iterate through the remaining characters.
+        if (line[i] >= '0' && line[i] <= '9') {
+            currentCount *= 10;
+            currentCount += (line[i] - '0');
+        } else if (line[i] == ',' || line[i] == ';' || line[i] == '\0' || line[i] == '\n') {
+            // Check what the last 3/4/5 chars are
+            if (strncmp(&line[i - 3], "red", 3) == 0) {
+                if (currentCount > maxRed)
+                    maxRed = currentCount;
+            } else if (strncmp(&line[i - 4], "blue", 4) == 0) {
+                if (currentCount > maxBlue)
+                    maxBlue = currentCount;
+            } else if (strncmp(&line[i - 5], "green", 5) == 0) {
+                if (currentCount > maxGreen)
+                    maxGreen = currentCount;
+            }
+            currentCount = 0;
+        }
+    }
+    
+    return (maxRed * maxBlue * maxGreen);
+}
+
+int main()
+{
+    FILE *fp = fopen(INPUT_FILE, "r");
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    int sum = 0;
+    
+    while ((read = getline(&line, &len, fp)) != -1) {
+        sum += read_game(line, read);
+    }
+
+    printf("Total: %d\n", sum);
+
+    return 0;
+}
