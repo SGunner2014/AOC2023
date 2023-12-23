48b28ec8b7917a0b6274b39a436a30f4a89c1552
Sam Gunner <me@sgunner.me>
2023-12-23 14:52:13 -0800

    Day 5 part 2

--- day5/part2.c
+++ day5/part2.c
@@ -5,8 +5,8 @@
 #define INPUT_FILE "input.txt"
 #define EXAMPLE_FILE "example.txt"
 #define EXPECTED_EXAMPLE_VALUE 46
-#define SKIP_EXAMPLE 0
-#define SKIP_ACTUAL 1
+#define SKIP_EXAMPLE 1
+#define SKIP_ACTUAL 0
 
 typedef struct RangeMap {
     long dest;
@@ -17,33 +17,10 @@ typedef struct RangeMap {
 typedef struct Seed {
     long start;
     long end;
+    int isMapped;
 } Seed;
 
-// Read in the list of seeds
-void readSeeds(char *line, ssize_t read, long **seeds, long **seedsCopy, int *seedsLength)
-{
-    int i, j;
-    j = 0;
-
-    long currentNum = 0;
-
-    for (i = 0; i < read; i++) {
-        if ((line[i] == ' ' || line[i] == '\n') && currentNum != 0) {
-            (*seeds)[j] = currentNum;
-            (*seedsCopy)[j] = currentNum;
-            currentNum = 0;
-            j++;
-        } else if (line[i] >= '0' && line[i] <= '9') {
-            currentNum *= 10;
-            currentNum += (line[i] - '0');
-        }
-    }
-
-    *seedsLength = j;
-    printf("Found %d seeds\n", *seedsLength);
-}
-
-void readSeedsNew(char *line, ssize_t read, Seed **seeds, int *seedsLength)
+void readSeeds(char *line, ssize_t read, Seed **seeds, int *seedsLength)
 {
     int i, j;
     j = 0;
@@ -56,6 +33,7 @@ void readSeedsNew(char *line, ssize_t read, Seed **seeds, int *seedsLength)
                 (*seeds)[j / 2].start = currentNum;
             } else {
                 (*seeds)[j / 2].end = (*seeds)[j / 2].start + currentNum - 1;
+                (*seeds)[j / 2].isMapped = 0;
             }
 
             currentNum = 0;
@@ -68,9 +46,6 @@ void readSeedsNew(char *line, ssize_t read, Seed **seeds, int *seedsLength)
 
     *seedsLength = j / 2;
     printf("Found %d seeds\n", *seedsLength);
-    for (i = 0; i < *seedsLength; i++) {
-        printf("%ld -> %ld\n", (*seeds)[i].start, (*seeds)[i].end);
-    }
 }
 
 // Read in and parse a range from a string
@@ -109,30 +84,7 @@ RangeMap readRange(char *line, ssize_t read)
     return range;
 }
 
-long applyMapping(RangeMap *mappings, int rangeMapLength, long seed)
-{
-    // Work through the mappings and see if there's one that fits
-    // Otherwise, return the seed as-is.
-    int i;
-    RangeMap currentMapping;
-    long difference;
-
-    for (i = 0; i < rangeMapLength; i++) {
-        currentMapping = mappings[i];
-
-        if (seed >= currentMapping.origin && seed <= (currentMapping.origin + currentMapping.length)) {
-            difference = seed - currentMapping.origin;
-
-            printf("Seed %ld -> %ld (%ld, %ld, %ld)\n", seed, currentMapping.dest + difference, currentMapping.dest, currentMapping.origin, currentMapping.length);
-
-            return currentMapping.dest + difference;
-        }
-    }
-
-    return seed;
-}
-
-void applyMappingsNew(RangeMap *mappings, int rangeMapLength, Seed **seeds, int *seedsLength)
+void applyMappings(RangeMap *mappings, int rangeMapLength, Seed **seeds, int *seedsLength)
 {
     // Work through the mapping and see if there's one that fits
     // If we need to split a seed range, we'll realloc and append a new range
@@ -148,10 +100,11 @@ void applyMappingsNew(RangeMap *mappings, int rangeMapLength, Seed **seeds, int 
         map_high = mappings[i].origin + mappings[i].length - 1;
         map_diff = mappings[i].dest - mappings[i].origin;
 
-        printf("Map diff: %ld (%ld -> %ld)\n", map_diff, map_low, map_high);
-
         // Iterate through seed ranges
-        for (j = 0; j < beginSeedsLength; j++) {
+        for (j = 0; j < (*seedsLength); j++) {
+            if ((*seeds)[j].isMapped) // Only check seed ranges that haven't already been mapped to new values
+                continue;
+
             // For each seed range:
             // Find if the seed range overlaps with the mapping at all
             // If it does, generate a new seed range for the mapped areas and
@@ -167,20 +120,20 @@ void applyMappingsNew(RangeMap *mappings, int rangeMapLength, Seed **seeds, int 
             over_low = (seed_low > map_low) ? seed_low : map_low;
             over_high = (seed_high < map_high) ? seed_high : map_high;
 
-            // printf("Detected overlap (%ld, %ld) from (%ld, %ld) and (%ld, %ld)\n", over_low, over_high, seed_low, seed_high, map_low, map_high);
-
             // Using this overlap, we can now generate seed ranges as required
             // Case 1 - overlap is exactly the same as the original range
             if (over_low == seed_low && over_high == seed_high) {
                 (*seeds)[j].start += map_diff;
                 (*seeds)[j].end += map_diff;
+                (*seeds)[j].isMapped = 1;
             // Case 2 - overlap is at the start of the original range
             } else if (over_low == seed_low && over_high < seed_high) {
                 (*seeds)[j].start += map_diff;
                 (*seeds)[j].end = (over_high + map_diff);
+                (*seeds)[j].isMapped = 1;
 
                 // Add another seed range
-                *seeds = realloc(*seeds, sizeof(Seed) * (*seedsLength) + 1);
+                *seeds = realloc(*seeds, sizeof(Seed) * ((*seedsLength) + 1));
                 (*seedsLength) += 1;
 
                 (*seeds)[*seedsLength - 1].start = over_high + 1;
@@ -189,22 +142,22 @@ void applyMappingsNew(RangeMap *mappings, int rangeMapLength, Seed **seeds, int 
             } else if (over_high == seed_high && over_low > seed_low) {
                 (*seeds)[j].start = (over_low + map_diff);
                 (*seeds)[j].end += map_diff;
+                (*seeds)[j].isMapped = 1;
 
                 // Add another seed range
-                *seeds = realloc(*seeds, sizeof(Seed) * (*seedsLength) + 1);
+                *seeds = realloc(*seeds, sizeof(Seed) * ((*seedsLength) + 1));
                 (*seedsLength) += 1;
 
                 (*seeds)[*seedsLength - 1].start = seed_low;
                 (*seeds)[*seedsLength - 1].end = over_low - 1;
             // Case 4 - overlap is somewhere in the middle of the original range
             } else if (over_high < seed_high && over_low > seed_low) {
-                printf("--- here ---\n");
-
                 (*seeds)[j].start = (over_low + map_diff);
                 (*seeds)[j].end = (over_high + map_diff);
+                (*seeds)[j].isMapped = 1;
 
                 // Add 2 more seed ranges
-                *seeds = realloc(*seeds, sizeof(Seed) * (*seedsLength) + 2);
+                *seeds = realloc(*seeds, sizeof(Seed) * ((*seedsLength) + 2));
                 (*seedsLength) += 2;
 
                 (*seeds)[*seedsLength - 2].start = seed_low;
@@ -214,6 +167,11 @@ void applyMappingsNew(RangeMap *mappings, int rangeMapLength, Seed **seeds, int 
                 (*seeds)[*seedsLength - 1].end = seed_high;
             }
         }
+    }
+
+    // Iterate through seeds and reset isMapped = false
+    for (i = 0; i < (*seedsLength); i++) {
+        (*seeds)[i].isMapped = 0;
     }
 }
 
@@ -232,7 +190,6 @@ long task(char *fileName)
 
     // Define a list of range maps
     // We assume that the flow of data is as follows: 1 -> 2 -> ... -> n - 1 -> n
-    // RangeMap **rangeMaps = (RangeMap**) malloc(sizeof(RangeMap) * 10 * 50);
     RangeMap rangeMaps[10][50];
     int rangeMapLengths[10];
 
@@ -248,8 +205,7 @@ long task(char *fileName)
     // Read the file in
     while ((read = getline(&line, &len, fp)) != EOF) {
         if (lineNo == 0) {
-            // readSeeds(line, read, &seeds, &seedsCopy, &seedsLength);
-            readSeedsNew(line, read, &seeds, &seedsLength);
+            readSeeds(line, read, &seeds, &seedsLength);
         } else {
             if (strncmp(line, "\n", 1) == 0) { // If it's a blank line, then we must have exited a range (unless we weren't in one!)
                 if (isInRange) {
@@ -262,7 +218,6 @@ long task(char *fileName)
                 isInRange = 1;
             } else if (isInRange) {
                 currentRange = readRange(line, read);
-                // printf("Current range no: %d, range inner no: %d\n", currentRangeNo, currentRangeInnerNo);
                 rangeMaps[currentRangeNo][currentRangeInnerNo] = currentRange;
                 currentRangeInnerNo++;
             }
@@ -283,24 +238,14 @@ long task(char *fileName)
     printf("Seed ranges: %d\n", seedsLength);
     // Now, pass the seeds from earlier through each of the ranges and see what we end up with
     for (i = 0; i <= currentRangeNo; i++) {
-        printf("--- Range %d ---\n", i);
-        // for (j = 0; j < seedsLength; j++) {
-        //     // Apply the current mapping to this seed
-
-
-        //     // applyMapping(rangeMaps[i], rangeMapLengths[i], seedsCopy[j]);
-        // }
-        applyMappingsNew(rangeMaps[i], rangeMapLengths[i], &seeds, &seedsLength);
-        printf("Seed ranges: %d\n", seedsLength);
+        applyMappings(rangeMaps[i], rangeMapLengths[i], &seeds, &seedsLength);
     }
 
     long finalValue = -1;
 
     for (i = 0; i < seedsLength; i++) {
-        printf("(%ld -> %ld)\n", seeds[i].start, seeds[i].end);
         if (finalValue == -1 || seeds[i].start < finalValue) {
             finalValue = seeds[i].start;
-            printf("Setting final value: %ld (%d)\n", seeds[i].start, i);
         }
     }
 
